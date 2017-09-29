566eaeb3c82a82aeb531620595c2a2275dc7e561
Dave Taylor <davehtaylor@me.com>
2017-09-29 16:29:17 -0400

    Getting started on refactoring. Cleaned up the type declarations, and removed a few functions. Will be reworking some of the existing fucntions and adding addition list operations.

--- linked_list.c
+++ linked_list.c
@@ -1,186 +1,107 @@
 #include <stdio.h>
-#include <stdlib.h>     // for malloc(), free()
+#include <stdlib.h>         // for malloc(), free()
+#include "linked_list.h"    // our custom header file
 
 
-// Create a new node in the list
-Node* create_node(int data, Node* next) 
+// Create a node
+Node *create_node(int data) 
 {
-    Node* new_node = (Node*)malloc(sizeof(Node));
+    Node *new_node = malloc(sizeof(Node));
 
     // Make sure we were able to allocate the memory
     if (new_node != NULL) 
     {
         new_node->data = data;
-        new_node->next = next;
+        new_node->next = NULL;
+        return new_node;
     }
     else 
     {
         fprintf(stderr, "Could not allocate memory");
-        exit(0);
+        return NULL;
     }
-
-    return new_node;
 }
 
 
 // Add an element to the end of the list
-Node* append_node(Node* head, int data) 
+Node *append_node(Node *head, int data) 
 {
-    // Make sure we're not trying to append after NULL
+    Node *cursor;         
+
+    // Check to see if list is empty. If so, we'll create a new node.
     if (head == NULL) 
     {
-        return NULL;
+        return create_node(data);
     }
     else 
     {
-        Node* cursor = head;
-        
         // Go to the end of the list
-        while (cursor->next != NULL) 
-            cursor = cursor->next;
+        for (cursor = head; cursor->next != NULL; cursor = cursor->next)
+            ;
 
-        Node* new_node = create_node(data, NULL);
-        cursor->next = new_node;
+        cursor->next = create_node(data);
+
         return head;
     }
 }
 
 
 // Delete head from list
-Node* delete_head(Node* head) 
+Node *delete_head(Node *head) 
 {
+    Node *first = head;
+    
     // Make sure the list isn't empty
     if (head == NULL) 
         return NULL;
     
-    // Otherwise, remove the head
-    Node* first = head;
-    head = head->next;
-    first->next = NULL;
-
-    // If the head is the only node in the list, set head to NULL
-    if (first == head) 
-        head = NULL;
-    
-    // Free the deleted node's memory
-    free(first);
-
-    return head;
+    // If the head is the only node in the list, just free it
+    else if (head->next == NULL) 
+    {
+        free(head);
+        return NULL;
+    }
+    else 
+    {
+        head = head->next;
+        free(first);
+        return head;
+    }
 }
 
 
 // Delete last node from list
-Node* delete_last_node(Node* head) 
+Node *delete_tail(Node *head) 
 {
+    Node *cursor, *temp;
+
     // Make sure list isn't empty
     if (head == NULL) 
+    {
         return NULL;
-
-    // Otherwise, go to the end of the list
-    Node* cursor = head;
-    Node* last = NULL;
-
-    while (cursor->next != NULL) {
-        last = cursor;
-        cursor = cursor->next;
     }
 
-    // Remove the last node
-    if (last != NULL) 
-        last->next = NULL;
-
-    // If we're deleting the last node in the list, set head to NULL
-    if (cursor == head) 
-        head = NULL;
-
-    // Free the deleted node's memory
-    free(cursor);
-
-    return head;
-}
-
-
-// Delete any node from the list 
-Node* delete_any_node(Node* head, Node* node_to_delete) 
-{
-    // Check to see if the list is empty
-    if (node_to_delete == NULL) 
+    // See if head is the only node in the list
+    else if (head->next == NULL)
+    {
+        free(head); 
         return NULL;
-
-    // Check to see if the node is the first node
-    if (node_to_delete == head) 
-        return delete_head(head);
-
-    // Check to see if the node is the last node
-    if (node_to_delete->next == NULL) 
-        return delete_last_node(head);
-
-    /* If the node to delete is anywhere else in the list, walk the list until
-     * we come to the node just before the one we want to delete
-     */
-    Node* cursor = head;
-
-    while (cursor != NULL) 
-    {
-        if (cursor->next == node_to_delete) 
-            break;
-
-        cursor = cursor->next;
     }
-
-    // Now that the node to delete is the node after the cursor
-    if (cursor != NULL) 
+    else 
     {
-        // Set a temp node to the node to delete
-        Node* temp = cursor->next;
-        // Set the cursor->next pointer to the node after the next one
-        cursor->next = temp->next;
-        // Delete the temp node
-        temp->next = NULL;
-        free(temp);
+        // Go to the next to the last element in the list
+        for (cursor = head; cursor->next->next != NULL; cursor = cursor->next)
+            ;
+
+        free(cursor->next);
+        cursor->next = NULL;
+        return head;
     }
-
-    return head;
-}
-
-
-// Count occurrences of particular element in the list
-int count_occurrences(Node* head, int user_data) 
-{
-    Node* cursor = head;
-    int occurrences = 0;
-
-    while (cursor != NULL) 
-    {
-        if (cursor->data == user_data) 
-            ++occurrences;
-
-        cursor = cursor->next;
-    }
-
-    return occurrences;
-}
-
-
-// Delete all occurences of a number from the list
-Node* delete_all_occurrences(Node* head, int user_data) 
-{
-    Node* cursor = head;
-
-    while (cursor != NULL) 
-    {
-        if (cursor->data == user_data) 
-            cursor = delete_any_node(head, cursor);
-
-        cursor = cursor->next;
-    }
-
-    return head;
 }
 
 
 // Print the data in each node of the list
-void print_list(Node* head) 
+void print_list(Node *head) 
 {
     Node* cursor = head;
 
@@ -193,11 +114,9 @@ void print_list(Node* head)
 
 
 // Destroy a list
-void destroy_list(Node* head) 
+Node *destroy_list(Node *head) 
 {
-    // Start the cursor on the head
-    Node* cursor = head;
-    Node* temp;
+    Node *cursor, *temp;
     
     while (cursor != NULL) 
     {
@@ -207,4 +126,6 @@ void destroy_list(Node* head)
         // Move the cursor to the address of the next node stored in temp
         cursor = temp;
     }
+
+    return NULL;
 }
--- linked_list.h
+++ linked_list.h
@@ -1,4 +1,4 @@
-#ifndef
+#ifndef LINKED_LIST_H
 #define LINKED_LIST_H
 
 // Structure for our linked list
@@ -8,31 +8,22 @@ typedef struct Node
     struct Node* next;
 } Node;
 
-// Create a new node in the list
-Node* create_node(int data, Node* next); 
+// Create a new node
+Node *create_node(int data); 
 
 // Add an element to the end of the list
-Node* append_node(Node* head, int data); 
+Node *append_node(Node *head, int data); 
 
 // Delete head from list
-Node* delete_head(Node* head); 
+Node *delete_head(Node *head); 
 
 // Delete last node from list
-Node* delete_last_node(Node* head); 
-
-// Delete any node from the list 
-Node* delete_any_node(Node* head, Node* node_to_delete); 
-
-// Count occurrences of particular element in the list
-int count_occurrences(Node* head, int user_data);
-
-// Delete all occurences of a number from the list
-Node* delete_all_occurrences(Node* head, int user_data); 
+Node *delete_tail(Node *head); 
 
 // Print the data in each node of the list
-void print_list(Node* head); 
+void print_list(Node *head); 
 
 // Destroy a list
-void destroy_list(Node* head); 
+Node *destroy_list(Node *head); 
 
 #endif
